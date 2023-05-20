7c0cfe16e0b36cd513d3680203b0f9d3908d1871
eyadmgt <eyadmgt@gmail.com>
2023-05-20 23:30:12 +0300

    huhhhh

---
+++ src/products.js
@@ -0,0 +1,45 @@
+import cat1 from "./assets/cat1.jpeg";
+import cat2 from "./assets/cat2.jpeg";
+import cat3 from "./assets/cat3.jpeg";
+import cat4 from "./assets/cat4.jpeg";
+import cat5 from "./assets/cat5.jpeg";
+import cat6 from "./assets/cat6.jpeg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "The Dark",
+    price: 159.0,
+    productImage: cat1,
+  },
+  {
+    id: 2,
+    productName: "The King",
+    price: 99.0,
+    productImage: cat2,
+  },
+  {
+    id: 3,
+    productName: "The Gamer",
+    price: 49.0,
+    productImage: cat3,
+  },
+  {
+    id: 4,
+    productName: "The Warrior",
+    price: 79.0,
+    productImage: cat4,
+  },
+  {
+    id: 5,
+    productName: "The Toy",
+    price: 29.0,
+    productImage: cat5,
+  },
+  {
+    id: 6,
+    productName: "The Driver",
+    price: 45.0,
+    productImage: cat6,
+  },
+];
